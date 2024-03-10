6364030d119c99b1a940248f2450f1ec468e83f9
Marko Gusic <markoggusic@gmail.com>
2024-03-10 13:36:34 +0100

    Update tf code with apig configuration

--- iac/main.tf
+++ iac/main.tf
@@ -105,3 +105,39 @@ resource "aws_iam_role_policy_attachment" "dynamodb_access" {
   role       = aws_iam_role.main.name
   policy_arn = aws_iam_policy.dynamodb_access.arn
 }
+
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.arn
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.comments_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_rest_api" "comments_api" {
+  name        = "comments-api-${var.env}"
+  description = "API for Comments Lambda function"
+}
+
+resource "aws_api_gateway_resource" "comments_resource" {
+  rest_api_id = aws_api_gateway_rest_api.comments_api.id
+  parent_id   = aws_api_gateway_rest_api.comments_api.root_resource_id
+  path_part   = "comments-${var.env}"
+}
+
+resource "aws_api_gateway_method" "comments_method" {
+  rest_api_id   = aws_api_gateway_rest_api.comments_api.id
+  resource_id   = aws_api_gateway_resource.comments_resource.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "comments_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.comments_api.id
+  resource_id             = aws_api_gateway_resource.comments_resource.id
+  http_method             = aws_api_gateway_method.comments_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.main.invoke_arn
+}
